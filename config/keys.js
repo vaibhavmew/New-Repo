8d60a2e9f4f3eb62caafbd79e939efe4a934fec1
vaibhavmew <68279946+vaibhavmew@users.noreply.github.com>
2020-08-10 12:51:42 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://<InsertName>:<InsertPassword>@cluster0-bsqi2.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://<InsertName>:<InsertPassword>@cluster0-bsqi2.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
